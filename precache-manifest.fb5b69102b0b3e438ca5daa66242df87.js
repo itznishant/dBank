0447878f2b79f7868f63facab87664fe93a3ce8a
System Administrator <root@Nishants-MacBook-Air.local>
2021-08-07 23:33:24 +0530

    Updates

---
+++ precache-manifest.fb5b69102b0b3e438ca5daa66242df87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebef1f2c1bd2c25960ef46513f31d9e",
+    "url": "/dBank/index.html"
+  },
+  {
+    "revision": "252294621659e286b716",
+    "url": "/dBank/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "3df37d9ad1fbf2355869",
+    "url": "/dBank/static/css/main.d1edde73.chunk.css"
+  },
+  {
+    "revision": "252294621659e286b716",
+    "url": "/dBank/static/js/2.054e9184.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/dBank/static/js/2.054e9184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df37d9ad1fbf2355869",
+    "url": "/dBank/static/js/main.a868df37.chunk.js"
+  },
+  {
+    "revision": "08933f9deaa16bb1e391",
+    "url": "/dBank/static/js/runtime-main.e0b4889a.js"
+  },
+  {
+    "revision": "c8d65b7401e78330c0519fdf6a32a73a",
+    "url": "/dBank/static/media/defibank.c8d65b74.png"
+  }
+]);
